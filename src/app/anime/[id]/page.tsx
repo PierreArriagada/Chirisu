1b960b3eaef20d6b291134535d8bd8c5675d85d7
Pierre Arriagada <arriagadapierre@gmail.com>
2025-10-01 23:46:57 +0000

    entonces, en mi pagina principal, cuando se muestren por ejemplo los ult

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,5 +1,26 @@
 import MediaPage from "@/components/media-page";
 import { getMediaPageData } from "@/lib/db";
+import type { Metadata } from 'next';
+
+type Props = {
+  params: { id: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const mediaData = getMediaPageData(params.id, 'anime');
+
+  if (!mediaData) {
+    return {
+      title: 'Contenido no encontrado',
+      description: 'La página que buscas no existe.',
+    }
+  }
+
+  return {
+    title: `${mediaData.titleInfo.title} | AniHub Info`,
+    description: mediaData.titleInfo.description,
+  }
+}
 
 export default function Page({ params }: { params: { id: string } }) {
   const mediaData = getMediaPageData(params.id, 'anime');
