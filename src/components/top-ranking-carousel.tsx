65c132f290970ad74f1b423ffe23bd9875759d74
Pierre Arriagada <arriagadapierre@gmail.com>
2025-10-02 17:10:46 +0000

    la imagen esmuy grande, deben verse en una resolcion de 340px de ancho d

--- src/components/top-ranking-carousel.tsx
+++ src/components/top-ranking-carousel.tsx
@@ -64,7 +64,7 @@ export default function TopRankingCarousel({ title, items, viewMoreLink }: TopRa
       >
         <CarouselContent className="-ml-2 pl-4 sm:pl-6 lg:pl-8">
            {items.map((item, index) => (
-            <CarouselItem key={index} className="basis-[60%] ss:basis-[45%] sm:basis-[40%] md:basis-[30%] lg:basis-[22%] pl-2">
+            <CarouselItem key={index} className="basis-[27%] ss:basis-[27%] sm:basis-[22%] md:basis-[18%] lg:basis-[15%] pl-2">
                <Link href={url(item)} className="block group cursor-pointer h-full">
                 <Card className="overflow-hidden h-full flex flex-col border-none shadow-lg bg-transparent">
                   <CardContent className="p-0 relative flex-grow">
