b3c091a7e308d7fa9cb26b98410282825e4af91b
Pierre Arriagada <arriagadapierre@gmail.com>
2025-10-02 15:08:11 +0000

    tambien mas cerca la separacoin de cada ranking, el top esta muy lejos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const mediaTypes: MediaType[] = ['Anime', 'Manga', 'Manhua', 'Manwha', 'Novela', 'Fan Comic', 'Dougua'];
 
   return (
-    <main className="space-y-12 my-8">
+    <main className="space-y-8 my-8">
       {mediaTypes.map(type => {
         const { topAllTime } = getMediaListPage(type);
         // Capitalize first letter of type for the link
