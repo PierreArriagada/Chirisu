13f97035549a905df2a3b487914e31fface82810
Pierre Arriagada <arriagadapierre@gmail.com>
2025-10-04 00:39:00 +0000

    era el layout el problema, ahora agrega un poco de padding sin centrar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             <SearchBar />
             <HorizontalMenu />
             <Breadcrumbs />
-            <main>
+            <main className="px-4 sm:px-6 lg:px-8">
               {children}
             </main>
             <Toaster />
